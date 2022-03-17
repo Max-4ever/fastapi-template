8e3469019911d9e5d70966a0e0abfe879736f8e2
Maksim Grafskiy <m.grafskiy@crpt.ru>
2022-03-17 23:02:35 +0300

    add summaries router

--- project/app/main.py
+++ project/app/main.py
@@ -2,15 +2,15 @@
 import logging
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
